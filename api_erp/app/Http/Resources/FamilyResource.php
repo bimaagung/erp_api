33b21e74e6b55f117462ef7cffc9ba062940f9db
bimaagung <bimaagung.labs@gmail.com>
2023-06-12 14:03:11 +0700

    fix: fill family resoure

--- api_erp/app/Http/Resources/FamilyResource.php
+++ api_erp/app/Http/Resources/FamilyResource.php
@@ -14,6 +14,15 @@ class FamilyResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'karyawan_id' => (int)$this->karyawan_id,
+            'status' => $this->status,
+            'nik' => (int)$this->nik,
+            'nama' => $this->jabatan,
+            'pekerjaan' => $this->pekerjaan,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+        ];
     }
 }
