b3946031dfc8f71cc71fb6b9bbe21b58b90bdff4
bimaagung <bimaagung.labs@gmail.com>
2023-06-15 14:17:14 +0700

    fix(add-karyawan): processing data insert and get all (karyawan, informasi personal, and informasi pekerjaan)

--- api_erp/app/Http/Resources/KaryawanResource.php
+++ api_erp/app/Http/Resources/KaryawanResource.php
@@ -15,6 +15,7 @@ class KaryawanResource extends JsonResource
     public function toArray($request)
     {
         return [
+            // Karyawan
             'id' => $request->id,
             'nama' => $this->nama,
             'nik' => (int)$this->nik,
@@ -28,6 +29,32 @@ class KaryawanResource extends JsonResource
             'tanggal_lahir' => $this->tanggal_lahir,
             'telp' => $this->telp,
             'foto' => $this->foto,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+
+            // Personal Information
+            'npwp' => $this->npwp,
+            'tipe_pajak' => $this->tipe_pajak,
+            'potongan_pajak' => (int)$this->potongan_pajak,
+            'tunjangan_pajak' => (int)$this->tunjangan_pajak,
+            'nama_bank' => $this->nama_bank,
+            'nomor_akun_bank' => (int)$this->nomor_akun_bank,
+            'bpjs_ketenagakerjaan' => (int)$this->bpjs_ketenagakerjaan,
+            'bpjs_kesehatan' => (int)$this->bpjs_kesehatan,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+
+            // Job Information
+            'kantor_cabang_id' => (int)$this->kantor_cabang_id,
+            'department' => $this->department,
+            'jabatan' => $this->jabatan,
+            'tanggal_masuk' => $this->tanggal_masuk,
+            'status' => $this->status,
+            'periode_kontrak' => (int)$this->periode_kontrak,
+            'potongan_terlambat' => $this->potongan_terlambat,
+            'toleransi_keterlambatan' => (int)$this->toleransi_keterlambatan,
+            'mode_absensi' => $this->mode_absensi,
+            'absen_diluar_kantor' => $this->absen_diluar_kantor,
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
         ];
