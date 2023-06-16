716d597439a015d8aa080f0c57dfd1fe553ced83
irvantaufik28 <irvantaufik28@gmail.com>
2023-06-16 13:32:46 +0700

    change template remove admin lte

--- fe_erp/src/pages/dashboard/DashboardPage.jsx
+++ fe_erp/src/pages/dashboard/DashboardPage.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import Header from '../../components/layouts/Header'
-import SideBar from '../../components/layouts/SideBar'
-import Footer from '../../components/layouts/Footer'
-import Content from '../../components/layouts/Content'
 const DashboardPage = () => {
   return (
     <>
-    <Header />
-    <SideBar />
-    <Content>
       <h1>example content</h1>
-    </Content>
-    <Footer />
     </>
   )
 }
