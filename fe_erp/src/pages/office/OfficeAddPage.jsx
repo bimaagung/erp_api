1374b48125ac0b055c74b67dbed84d5acd1906a2
irvantaufik28 <irvantaufik28@gmail.com>
2023-06-19 09:57:44 +0700

    fix form add kantor cabang

--- fe_erp/src/pages/office/OfficeAddPage.jsx
+++ fe_erp/src/pages/office/OfficeAddPage.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import SideBar from "../../components/layouts/Sidebar";
 import FormAddKantorCabang from "./components/FormAddKantorCabang";
 
+
+const handleSubmit = (payload) => {
+  console.log(payload)
+}
+
 const OfficeAddPage = () => {
   return (
     <SideBar>
-     <FormAddKantorCabang />
+     <FormAddKantorCabang onSubmit = {handleSubmit}/>
     </SideBar>
   );
 };
--- fe_erp/src/pages/office/components/FormAddKantorCabang.jsx
+++ fe_erp/src/pages/office/components/FormAddKantorCabang.jsx
@@ -1,43 +1,106 @@
 import React, { useState } from "react";
 import "../styles/formAddKantorCabang.css";
 import { Card } from "react-bootstrap";
-import { TimePickerComponent } from "@syncfusion/ej2-react-calendars";
+import { TimePicker } from 'antd';
+import dayjs from 'dayjs';
 
-function FormAddKantorCabang() {
-  const [selectedTime, setSelectedTime] = useState(
-    new Date("01/01/2021 08:30 AM")
-  );
-  const minTime = new Date("01/02/2021 09:00 AM");
-  const maxTime = new Date("01/02/2021 18:00 AM");
 
-  const handleTimeChange = (event) => {
-    setSelectedTime(event.value);
+const FormAddKantorCabang = (props) => {
+  
+  const format = 'HH:mm'; 
+  const [selectedMasukSeninJumat, setSelectedSeninJumat] = useState(dayjs('09:00', format));
+  const [selectedMasukSabtuMinggu, setSelectedMasukSabtuMinggu] = useState(dayjs('09:00', format));
+  const [selectedKeluarSeninJumat, setSelectedKeluarSeninJumat] = useState(dayjs('17:00', format));
+  const [selectedKeluarSabtuMinggu, setSelectedKeluarSabutMinggu] = useState(dayjs('17:00', format));
+  const [form,  setForm]= useState({
+    nama: "",
+    alamat: "",
+    phone1: "",
+    phone2: "",
+    jamMasukSeninJumat: (dayjs('09:00', format)).format(format),
+    jamMasukSabtuMinggu: (dayjs('09:00', format)).format(format),
+    jamKeluarSeninJumat: (dayjs('17:00', format)).format(format),
+    jamKeluarSabtuMinggu: (dayjs('17:00', format)).format(format)
+  })
+  const handlelMasukSeninJumat = (time) => {
+    if (time) {
+      setSelectedSeninJumat(time);
+      const formattedTime = time.format(format);
+      setForm({
+        ...form,
+        jamMasukSeninJumat: formattedTime,
+      });
+    }
+  };
+  
+  const handlelMasukSabtuMinggu = (time) => {
+    if (time) {
+      setSelectedMasukSabtuMinggu(time);
+      const formattedTime = time.format(format);
+      setForm({
+        ...form,
+        jamMasukSabtuMinggu: formattedTime,
+      });
+    }
   };
 
-  const formattedTime = selectedTime.toLocaleTimeString("en-US", {
-    hour12: false,
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-  console.log(formattedTime);
+const handlelKeluarSeninJumat = (time) => {
+    if (time) {
+      setSelectedKeluarSeninJumat(time);
+      const formattedTime = time.format(format);
+      setForm({
+        ...form,
+        jamKeluarSeninJumat: formattedTime,
+      });
+    }
+  };
+  const handlelKeluarSabtuMinggu = (time) => {
+    if (time) {
+      setSelectedKeluarSabutMinggu(time);
+      const formattedTime = time.format(format);
+      setForm({
+        ...form,
+        jamMasukSabtuMinggu: formattedTime,
+      });
+    }
+  };
+
+ 
   return (
     <Card style={{ width: "100%", height: "auto" }}>
       <Card.Body>
         <div className="title-add-kantor-cabang">Form Cabang</div>
         <hr></hr>
-        <form>
-          <div>
+        <form onSubmit={(e => {
+              e.preventDefault()
+              props.onSubmit(form)
+            })}>
+          <div >
             <div className="mb-3">
               <label htmlFor="nama" className="form-label">
                 Nama:
               </label>
-              <input type="text" className="form-control" id="nama" />
+              <input 
+              type="text" 
+              className="form-control" 
+              id="nama"
+              onChange={e => setForm({
+                ...form, ...{ nama: e.target.value }
+              })}
+              />
             </div>
             <div className="mb-3">
               <label htmlFor="Alamat" className="form-label">
                 Alamat:
               </label>
-              <input type="text" className="form-control" id="Alamat" />
+              <input 
+              type="text" 
+              className="form-control" 
+              id="Alamat" 
+              onChange={e => setForm({
+                ...form, ...{ alamat: e.target.value }
+              })}
+              />
             </div>
             <div className="row">
               <div className="col-md-6">
@@ -45,25 +108,36 @@ function FormAddKantorCabang() {
                   <label htmlFor="phone1" className="form-label">
                     phone1:
                   </label>
-                  <input type="text" className="form-control" id="phone1" />
+                  <input 
+                  type="text" 
+                  className="form-control" 
+                  id="phone1" 
+                  onChange={e => setForm({
+                    ...form, ...{ phone1: e.target.value }
+                  })}
+                  />
                 </div>
                 <label htmlFor="phone1" className="form-label">
-                    Jam Masuk (senin-jumat):
+                    Jam Masuk (senin-jumat  ):
                   </label>
-                <TimePickerComponent
-                  placeholder="Select a time"
-                  value={selectedTime}
-                  min={minTime}
-                  max={maxTime}
-                  format="HH:mm"
-                  step={60}
-                  onChange={handleTimeChange}
-                ></TimePickerComponent>
+                  <div className="time-picker">
+                  <TimePicker 
+                  onChange={handlelMasukSeninJumat} 
+                  format={format}
+                  defaultValue={selectedMasukSeninJumat} />
+
+                  </div>
                 <div className="mb-3">
                   <label htmlFor="phone1" className="form-label">
                     Jam Masuk (sabtu-Minggu):
                   </label>
-                  <input type="text" className="form-control" id="phone1" />
+                  <div className="time-picker">
+                  <TimePicker 
+                  onChange={handlelMasukSabtuMinggu} 
+                  format={format}
+                  defaultValue={selectedMasukSabtuMinggu} />
+
+                  </div>
                 </div>
               </div>
               <div className="col-md-6">
@@ -71,19 +145,38 @@ function FormAddKantorCabang() {
                   <label htmlFor="phone2" className="form-label">
                     phone2:
                   </label>
-                  <input type="text" className="form-control" id="phone2" />
+                  <input 
+                  type="text" 
+                  className="form-control"
+                   id="phone2" 
+                   onChange={e => setForm({
+                    ...form, ...{ phone2: e.target.value }
+                  })}
+                   />
                 </div>
                 <div className="mb-3">
                   <label htmlFor="phone1" className="form-label">
                     Jam Keluar (Senin-Jumat):
                   </label>
-                  <input type="text" className="form-control" id="phone1" />
+                  <div className="time-picker">
+                  <TimePicker 
+                  onChange={handlelKeluarSeninJumat} 
+                  format={format}
+                  defaultValue={selectedKeluarSeninJumat} />
+
+                  </div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="phone1" className="form-label">
                     Jam Keluar (sabtu-Minggu):
                   </label>
-                  <input type="text" className="form-control" id="phone1" />
+                  <div className="time-picker">
+                  <TimePicker 
+                  onChange={handlelKeluarSabtuMinggu} 
+                  format={format}
+                  defaultValue={selectedKeluarSabtuMinggu} />
+
+                  </div>
                 </div>
               </div>
             </div>
@@ -99,3 +192,8 @@ function FormAddKantorCabang() {
 }
 
 export default FormAddKantorCabang;
+
+FormAddKantorCabang.defaultProps = {
+  onSubmit: () => {}
+}
+
