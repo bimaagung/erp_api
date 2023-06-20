a7b0e3fd87c024cf2fdf68ef885873434c70dbac
irvantaufik28 <irvantaufik28@gmail.com>
2023-06-20 15:49:48 +0700

    some fix form input

---
+++ fe_erp/src/pages/employee/EmployeeAddPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideBar from '../../components/layouts/Sidebar'
+import FormAddEmployee from './components/FormAddEmployee'
+
+const EmployeeAddPage = () => {
+  return (
+    <SideBar>
+       <FormAddEmployee />
+    </SideBar>
+  )
+}
+
+export default EmployeeAddPage
