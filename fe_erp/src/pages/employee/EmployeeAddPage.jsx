015aa8842c8d9e9e92de3bc5c9729ef9cb7e0803
irvantaufik28 <irvantaufik28@gmail.com>
2023-07-04 12:22:32 +0700

    tabs karyawan

--- fe_erp/src/pages/employee/EmployeeAddPage.jsx
+++ fe_erp/src/pages/employee/EmployeeAddPage.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
 import SideBar from "../../components/layouts/Sidebar";
 import FormAddEmployee from "./components/FormAddEmployee";
 import { useDispatch } from "react-redux";
 import { addEmployee } from "../../features/employeeSlice";
 import { useNavigate } from "react-router-dom";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
+import { Tab, Tabs } from "react-bootstrap";
 const EmployeeAddPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -13,31 +13,50 @@ const EmployeeAddPage = () => {
     try {
       await dispatch(addEmployee(payload)).unwrap();
       Swal.fire({
-        title: 'Do you want to save the changes?',
+        title: "Do you want to save the changes?",
         showDenyButton: true,
         showCancelButton: true,
-        confirmButtonText: 'Save',
+        confirmButtonText: "Save",
         denyButtonText: `Don't save`,
-      }).then((result) => { 
+      }).then((result) => {
         if (result.isConfirmed) {
-          Swal.fire('Saved!', '', 'success')
+          Swal.fire("Saved!", "", "success");
           navigate("/admin/karyawan");
         } else if (result.isDenied) {
-          Swal.fire('Changes are not saved', '', 'info')
+          Swal.fire("Changes are not saved", "", "info");
         }
-      })
+      });
     } catch (error) {
-      Swal.fire(
-        'error',
-        error?.meta?.message,
-        'error'
-      )
+      Swal.fire("error", error?.meta?.message, "error");
     }
   };
 
   return (
     <SideBar>
-      <FormAddEmployee onSubmit={handleSubmit} />
+      <div>
+        <Tabs
+          defaultActiveKey="karyawan-form"
+          id="justify-tab-example"
+          className="mb-3"
+          justify
+        >
+          <Tab eventKey="karyawan-form" title="Informasi Umum">
+            <FormAddEmployee onSubmit={handleSubmit} />
+          </Tab>
+          <Tab eventKey="karyawan-pendidikan" title="Riwayat Pendidikan">
+            <h1>Riwayat Pendidikan</h1>
+          </Tab>
+          <Tab eventKey="karyawan-kerja" title="Riwayat Kerja">
+            <h1>Riwayat</h1>
+          </Tab>
+          <Tab eventKey="karyawan-keluarga" title="Data Keluarga">
+            <h1>Data Keluarga</h1>
+          </Tab>
+          <Tab eventKey="karyawan-pelatihan" title="Data Pelatihan">
+            <h1>Data Pelatihan</h1>
+          </Tab>
+        </Tabs>
+      </div>
     </SideBar>
   );
 };
